bd81269e8f14b3cf043f374e8eefc6c596774eb4
leticiavitoria17 <leticiavitoria170306@gmail.com>
2023-06-08 11:35:25 -0300

    PrimeiroCommite

---
+++ AppleStoreTupinikim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppleStoreTupinikim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
